c391ee3435b9fb11cd104b155d447d6a5df873f3
Cristian Venegas <cristian.venegas@materoceano.com>
2022-07-21 00:06:51 -0400

    fix : mayor problems

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,115 +1,103 @@
 //#region Arreglo
-const propiedades = [
+const propiedadesJSON = [
   {
-    tipo: "Departamento",
-    descripcion: "Desde las alturas todo se ve mejor",
-    src: "../assets/img/depto1.jpg",
-    habitaciones: 1,
-    metros: 35,
+    name: "Casa de campo",
+    description: "Un lugar ideal para descansar de la ciudad",
+    src: "https://www.construyehogar.com/wp-content/uploads/2020/02/Dise%C3%B1o-casa-en-ladera.jpg",
+    rooms: 2,
+    m: 170,
   },
   {
-    tipo: "Departamento",
-    descripcion: "Desde las alturas todo se ve mejor",
-    src: "../assets/img/depto2.jpg",
-    habitaciones: 2,
-    metros: 55,
+    name: "Casa de playa",
+    description: "Despierta tus días oyendo el oceano",
+    src: "https://media.chvnoticias.cl/2018/12/casas-en-la-playa-en-yucatan-2712.jpg",
+    rooms: 2,
+    m: 130,
   },
   {
-    tipo: "Casa",
-    descripcion: "Desde las alturas todo se ve mejor",
-    src: "../assets/img/casa1.jpg",
-    habitaciones: 3,
-    metros: 120,
+    name: "Casa en el centro",
+    description: "Ten cerca de ti todo lo que necesitas",
+    src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+    rooms: 1,
+    m: 80,
   },
   {
-    tipo: "Casas",
-    descripcion: "Desde las alturas todo se ve mejor",
-    src: "../assets/img/casa2.jpg",
-    habitaciones: 4,
-    metros: 180,
+    name: "Casa rodante",
+    description: "Conviertete en un nómada del mundo sin salir de tu casa",
+    src: "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
+    rooms: 1,
+    m: 6,
   },
   {
-    tipo: "Departamento",
-    descripcion: "Desde las alturas todo se ve mejor",
-    src: "../assets/img/depto3.jpg",
-    habitaciones: 3,
-    metros: 200,
+    name: "Departamento",
+    description: "Desde las alturas todo se ve mejor",
+    src: "https://www.adondevivir.com/noticias/wp-content/uploads/2016/08/depto-1024x546.jpg",
+    rooms: 3,
+    m: 200,
   },
   {
-    tipo: "Departamento",
-    descripcion: "Desde las alturas todo se ve mejor",
-    src: "../assets/img/depto4.jpg",
-    habitaciones: 1,
-    metros: 36,
-  },
-  {
-    tipo: "Casa",
-    descripcion: "Desde las alturas todo se ve mejor",
-    src: "../assets/img/casa3.jpg",
-    habitaciones: 3,
-    metros: 90,
-  },
-
-  {
-    tipo: "Departamento",
-    descripcion: "Desde las alturas todo se ve mejor",
-    src: "../assets/img/casa4.jpg",
-    habitaciones: 2,
-    metros: 60,
+    name: "Mansión",
+    description: "Vive una vida lujosa en la mansión de tus sueños ",
+    src: "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+    rooms: 5,
+    m: 500,
   },
 ];
-
 //#endregion
 
 //variables
-let result = document.querySelector(".container-result");
+let result = document.querySelector(".propiedades");
+
+
+const template = (prop) => {
+  console.log(prop);
+  return /*html*/ `
+    <div class="propiedad">
+    <div class="img" style="background-image: url('${prop.src}')"></div>
+    <section>
+      <h5>${prop.name}</h5>
+      <div class="d-flex justify-content-between">
+        <p>Cuartos: ${prop.rooms}</p>
+        <p>Metros: ${prop.m}</p>
+        </div>
+        <p class="my-3">${prop.description}</p>
+        <button class="btn btn-info ">Ver más</button>
+    </section>
+  </div>
+  `;
+};
 
 const cargaDatosPropiedades = () => {
-  let html = ""
-  let cantidad = ""
-  for (const propiedad of propiedades) {
-    cantidad++, html+=/*html*/`<div class="card" style="width: 18rem;">
-    <img src="${propiedad.src}" class="card-img-top" alt="imagen propiedad">
-    <div class="card-body">
-      <h5 class="card-title">${propiedad.tipo}</h5>
-      <h6 class="card-subtitle mb-2 text-muted">Nº Habitaciones: ${propiedad.habitaciones}</h6>
-      <h6 class="card-subtitle mb-2 text-muted">Metros:${propiedad.metros} M<span>&#178;</span></h6>
-      <p class="card-text">${propiedad.descripcion}.</p>
-      <a href="#" class="btn btn-primary">Ver más</a>
-    </div>
-  </div>`
-    
+  let cantidad = "";
+  for (const propiedad of propiedadesJSON) {
+    let html = template(propiedad);
+    result.innerHTML += html;
   }
-  result.innerHTML = html
   console.log(cantidad);
-}
+};
 
 const buscarPropiedadPorMetroCuadrado = () => {
   let minText = document.querySelector(".min").value;
   let maxText = document.querySelector(".max").value;
+  let rooms = document.querySelector(".rooms").value;
   let html = "";
-  let cantidad = ""
 
-  for (const propiedad of propiedades) {
-    if (propiedad.metros >= minText && propiedad.metros <= maxText) {
-      cantidad++, html += /*html*/ `
-    <div class="card" style="width: 18rem;">
-  <img src="${propiedad.src}" class="card-img-top" alt="imagen propiedad">
-  <div class="card-body">
-    <h5 class="card-title">${propiedad.tipo}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Nº Habitaciones: ${propiedad.habitaciones}</h6>
-    <h6 class="card-subtitle mb-2 text-muted">Metros:${propiedad.metros} M<span>&#178;</span></h6>
-    <p class="card-text">${propiedad.descripcion}.</p>
-    <a href="#" class="btn btn-primary">Ver más</a>
-  </div>
-</div> 
+  if(minText == 0 && maxText == 0 && rooms == 0) {
+    alert("Debes ingresar todos los valores")
+    return
+  }
 
-    `;
-    }
+  let filtro = propiedadesJSON.filter((p) => p.m >= minText && p.m <= maxText && p.rooms >= rooms
+  );
+
+  //limpiar pantalla
+  result.innerHTML = "";
+  for (const f of filtro) {
+    html += template(f);
   }
   result.innerHTML = html;
-  console.log(cantidad);
 };
 
+document.addEventListener("DOMContentLoaded", cargaDatosPropiedades);
 
-document.addEventListener("DOMContentLoaded", cargaDatosPropiedades)
+
