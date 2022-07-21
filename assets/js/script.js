b72a913a72a404d9a269835a21c52cc5370e07cb
Cristian Venegas <cristian.venegas@materoceano.com>
2022-07-21 18:28:25 -0400

    fix : ui 2.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,7 @@ const propiedadesJSON = [
   },
   {
     name: "Casa rodante",
-    description: "Conviertete en un nómada del mundo sin salir de tu casa",
+    description: "Conviertete en un nómada del mundo ",
     src: "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
     rooms: 1,
     m: 6,
@@ -37,35 +37,60 @@ const propiedadesJSON = [
   },
   {
     name: "Mansión",
-    description: "Vive una vida lujosa en la mansión de tus sueños ",
+    description: "Vive una vida lujosa en la mansión vip de tus sueños ",
     src: "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
     rooms: 5,
     m: 500,
+  },
+  {
+    name: "Departamento",
+    description: "Vive una vida lujosa en el departamento de tus sueños ",
+    src: "https://http2.mlstatic.com/D_NQ_NP_2X_786208-MLC50785462705_072022-O.webp",
+    rooms: 5,
+    m: 500,
+  },
+  {
+    name: "Departamento",
+    description: "Vive una vida lujosa en el departamento de tus sueños ",
+    src: "https://http2.mlstatic.com/D_NQ_NP_2X_852663-MLC50775597688_072022-O.webp",
+    rooms: 3,
+    m: 200,
+  },
+  {
+    name: "Casa de campo",
+    description: "Vive una vida lujosa en la casa de capo de tus sueños ",
+    src: "https://a0.muscache.com/im/pictures/997261b3-1645-4766-bb72-dae4caca150c.jpg?im_w=720",
+    rooms: 5,
+    m: 500,
+  },
+  {
+    name: "Isla",
+    description: "Disfruta de nuestra isla privada ubicada en del Rosario.  ",
+    src: "https://a0.muscache.com/im/pictures/miso/Hosting-49227437/original/7cf358b9-e8c5-4067-9ac5-014784387e22.jpeg?im_w=720",
+    rooms: 5,
+    m: 1750,
   },
 ];
 //#endregion
 
 //variables
-let result = document.querySelector(".propiedades");
-let contador = document.querySelector(".contador")
-
+let result = document.querySelector(".realState-card");
+let contador = document.querySelector(".contador");
 
 
 const template = (prop) => {
   console.log(prop);
   return /*html*/ `
-    <div class="propiedad">
-    <div class="img" style="background-image: url('${prop.src}')"></div>
-    <section>
-      <h5>${prop.name}</h5>
-      <div class="d-flex justify-content-between">
-        <p>Cuartos: ${prop.rooms}</p>
-        <p>Metros: ${prop.m}</p>
-        </div>
-        <p class="my-3">${prop.description}</p>
-        <button class="btn btn-info ">Ver más</button>
-    </section>
-  </div>
+        <div class="card" style="width: 15rem">
+            <img src="${prop.src}" class="card-img-top" alt="imagen Propiedad" height="158px" width ="238px"  />
+            <div class="card-body">
+              <h5 class="titulo"><b>${prop.name}</b></h5>
+              <h5 class="m2 subtitulo">${prop.m}<span> Metros&#178;</span></h5>
+              <h5 class="rooms subtitulo">${prop.rooms} <span>habitaciones</span> </h5>
+              <p class="card-text description">${prop.description}</p>
+              <a href="#" class="btn btn-primary">Más Informacion</a>
+            </div>
+          </div>
   `;
 };
 
@@ -73,11 +98,11 @@ const cargaDatosPropiedades = () => {
   let cantidad = "";
   for (const propiedad of propiedadesJSON) {
     let html = template(propiedad);
-    cantidad ++
+    cantidad++;
     result.innerHTML += html;
   }
   console.log(cantidad);
-  contador.innerHTML = cantidad
+  contador.innerHTML = cantidad;
 };
 
 const buscarPropiedadPorMetroCuadrado = () => {
@@ -85,29 +110,28 @@ const buscarPropiedadPorMetroCuadrado = () => {
   let maxText = document.querySelector(".max").value;
   let rooms = document.querySelector(".rooms").value;
   let html = "";
-  cantidad = 0
+  cantidad = 0;
 
-  if(minText == 0 || maxText == 0 || rooms == 0) {
-    alert("Debes ingresar todos los valores")
+  if (minText == 0 || maxText == 0 || rooms == 0) {
+    alert("Debes ingresar todos los valores");
   }
 
-  let filtro = propiedadesJSON.filter((p) => p.m >= minText && p.m <= maxText && p.rooms >= rooms
+  let filtro = propiedadesJSON.filter(
+    (p) => p.m >= minText && p.m <= maxText && p.rooms >= rooms
   );
 
-  if(filtro.length==0){
-    html+= `<h1>Busqueda sin resultados</h1>`
+  if (filtro.length == 0) {
+    html += `<h1>Busqueda sin resultados</h1>`;
   }
 
   //limpiar pantalla
   result.innerHTML = "";
   for (const f of filtro) {
     html += template(f);
-    cantidad++
+    cantidad++;
   }
   result.innerHTML = html;
-  contador.innerHTML = cantidad
+  contador.innerHTML = cantidad;
 };
 
 document.addEventListener("DOMContentLoaded", cargaDatosPropiedades);
-
-
